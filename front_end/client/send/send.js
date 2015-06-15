681fe6de0f987ba25e93c0a16c41aa640e7d0c30
Firescar96 <nchinda2@gmail.com>
2015-06-15 02:41:41 -0400

    old code removal

--- front_end/client/send/send.js
+++ front_end/client/send/send.js
@@ -46,23 +46,18 @@ Template.send.rendered = function() {
 
   web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
   var coinbase = web3.eth.coinbase;
-  console.log(coinbase);
   $('#coinbase').text(coinbase);
   var balance = web3.eth.getBalance(coinbase);
-  console.log(balance.toString(10));
   $('#balance').text(web3.toDecimal(balance));
 
   $('#send-button').click(function() {
-    console.log("Here we go")
     web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
     var coinbase = web3.eth.coinbase;
     var InterfaceContract = web3.eth.contract(interface_abi);
-    console.log(InterfaceContract)
     var amount = $("#amount").val();
-    console.log(amount)
 
-    window.interface_contract = InterfaceContract.at('0x9847fb68d77fcfc2699df45392388f56498793ba');
-    window.interface_contract.send_ether.sendTransaction(
+    interface_contract = InterfaceContract.at('0x9847fb68d77fcfc2699df45392388f56498793ba');
+    interface_contract.send_ether.sendTransaction(
       amount,
       { from: coinbase,
         value: amount,
