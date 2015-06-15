a50e5044fe70a383c35bf632aed209e892ad86f7
Firescar96 <nchinda2@gmail.com>
2015-06-14 22:53:27 -0400

    merge

--- front_end/client/join/join.js
+++ front_end/client/join/join.js
@@ -2,8 +2,8 @@ Router.route('/join', function() {
   this.render('join')});
 
 function store(){
-  var input_name = $("#username");
-  localStorage.setItem("user_id", input_name.value);
+  var input_name = $("#username").val();
+  localStorage.setItem("user_id", input_name);
   var storedValue = localStorage.getItem("user_id");
   console.log(storedValue);
 }
